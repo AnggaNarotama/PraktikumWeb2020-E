201d0f69f7bab2e6ec4dde6c6811b1b1963621dd
Angga Narotama <39909489+AnggaNarotama@users.noreply.github.com>
2020-12-18 23:46:50 +0800

    Add files via upload

---
+++ UAS/payment_success.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+		<head>
+			<meta charset="UTF-8">
+			<title>Thankyou</title>
+			<link rel="stylesheet" href="css/bootstrap.min.css"/>
+			<script src="js/jquery2.js"></script>
+			<script src="js/bootstrap.min.js"></script>
+			<script src="main.js"></script>
+			<style>
+				table tr td {padding:10px;}
+			</style>
+		</head>
+	<body>
+		<p><br/></p>
+		<p><br/></p>
+		<div class="container-fluid">
+						
+			<div class="row">
+				<div class="col-md-2"></div>
+				<div class="col-md-8">
+					<div class="panel panel-default">
+						<div class="panel-heading"></div>
+							<div style="text-align: center;" class="panel-body">
+								<h1>Thankyou</h1>
+								<hr/>
+									<p>Hello, your item will be delivered soon, thanks for your money.</p>
+									<img src="http://www.simpleimageresizer.com/_uploads/photos/845bdc04/Pngtreelucky_fortune_more_than_a_3954008_20.png" alt="Thanks">
+								<br>
+								<br>
+								<a href="index.php" class="btn btn-success btn-lg">Continue Shopping</a>
+							</div>
+						<div class="panel-footer"></div>
+					</div>
+				</div>
+			<div class="col-md-2"></div>
+		</div>
+		</div>
+	</body>
+</html>
---
+++ UAS/profile.php
@@ -0,0 +1,16 @@
+<?php
+    include "header.php";
+?>
+
+<div>
+    <h3>Account Information</h3>
+    <hr>
+    <p>Order History</p>
+    <!-- Ambil database order disini -->
+</div>
+
+
+<?php
+    include "newslettter.php";
+    include "footer.php";
+?>
